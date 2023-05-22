075c453ceb70f7521ab61f3460bf2442af57cc24
NotBB <notice76610@gmail.com>
2023-05-22 15:40:40 +0200

    g

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
-$(document).ready(function() {
-  // Lorsque le bouton est cliqué
-  $('#myButton').click(function() {
-    // Utiliser Swal.alert pour afficher une alerte
-    Swal.alert({
+  // Récupérer la référence du bouton
+  let button = document.getElementById('button')
+
+  // Ajouter un gestionnaire d'événement pour le clic sur le bouton
+  button.addEventListener('click', function() {
+    // Utiliser Swal.fire pour afficher une alerte
+    Swal.fire({
       title: 'SweetAlert2',
       text: 'Ceci est une alerte avec SweetAlert2!',
       icon: 'success',
       confirmButtonText: 'OK'
+  })
     });
-  });
-});
